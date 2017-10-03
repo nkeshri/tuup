8616a4dc6781db7b1d18572220f5349e247bdd93
nkeshri <nimika.keshri@gmail.com>
2017-10-03 17:03:39 +0300

    Placeholder test files for other places API

---
+++ testPlacesStopId.py
@@ -0,0 +1,22 @@
+import os
+import unittest
+import sys
+from tuupapiutil import TuupPlacesApi
+
+host = "devapi.tuup.fi"
+apistopId = "places/v2/stops/{}"
+
+class ApiStopIdTest(unittest.TestCase):
+    def setUp(self):
+        self.client = TuupPlacesApi(host, apiNearby)
+
+    def test01(self):
+        self.client.addParams(at="61.44,23.8")
+        self.client.generateUrl()
+        self.client.getRequest()
+        status, data = self.client.parseJson()
+        print("The status is %s and the content is %s" %(status,data))
+
+if __name__ == "__main__":
+    unittest.main()
+
