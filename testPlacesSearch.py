671bcd808600ffc47d89a832bf949f672665a4da
nkeshri <nimika.keshri@gmail.com>
2017-10-06 15:50:11 +0300

    Adding tests for the search API

--- testPlacesSearch.py
+++ testPlacesSearch.py
@@ -8,14 +8,38 @@ apisearch = "places/v2/search"
 
 class ApiSearchTest(unittest.TestCase):
     def setUp(self):
-        self.client = TuupPlacesApi(host, apiNearby)
+        self.client = TuupPlacesApi(host, apisearch)
 
-    def test01(self):
-        self.client.addParams(at="61.44,23.8")
-        self.client.generateUrl()
-        self.client.getRequest()
+    def test01_onlyCharacters(self):
+        self.client.executeRequestWithParams(text="act")
         status, data = self.client.parseJson()
-        print("The status is %s and the content is %s" %(status,data))
+        self.assertEqual(status, 200, "Request Failed")
+        self.assertGreater(len(data['search']), 0, "No places found with the given search text")
+
+    def test02_onlynumbersWithExistingPlaces(self):
+        self.client.executeRequestWithParams(text="787")
+        status, data = self.client.parseJson()
+        self.assertEqual(status, 200, "Request Failed")
+        self.assertGreater(len(data['search']), 0, "No places found with the given search text")
+
+    def test03_onlynumbersWithNoExistingPlaces(self):
+        self.client.executeRequestWithParams(text="7282626")
+        status, data = self.client.parseJson()
+        self.assertEqual(status, 200, "Request Failed")
+        self.assertEqual(len(data['search']), 0, "places found with the given search text")
+
+    def test04_specialCharctes(self):
+        self.client.executeRequestWithParams(text="%&")
+        status, data = self.client.parseJson()
+        self.assertEqual(status, 200, "Request Failed")
+        self.assertEqual(len(data['search']), 0, "places found with the given search text")
+
+    def test05_textAndLocation(self):
+        self.client.executeRequestWithParams(text="nimika", at="61.44, 23.8")
+        status, data = self.client.parseJson()
+        self.assertEqual(status, 200, "Request Failed")
+        self.assertGreater(len(data['search']), 0, "No places found with the given search text")
+
 
 if __name__ == "__main__":
     unittest.main()
